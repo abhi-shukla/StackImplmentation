81d1436915bee202ae8efcb763777230640bbfdd
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-04 23:52:50 -0700

    Add Stack using Array Implementation

---
+++ StackApp/MyArrayStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    public class MyArrayStack<T> : IEnumerable<T>
+    {
+        T[] _list = new T[0];
+
+        int _size = 0;
+
+        public void Push(T item)
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StackApp/MyLLStack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackApp
+{
+    public class MyLLStack<T> : IEnumerable<T>
+    {
+        LinkedList<T> MyStack = new LinkedList<T>();
+
+        public int Count
+        {
+            get 
+            {
+                return MyStack.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Push(T item)
+        {
+            MyStack.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if(MyStack.Count == 0)
+            {
+                throw new IndexOutOfRangeException("The stack is empty");
+            }
+            var item = MyStack.First.Value;
+            MyStack.RemoveFirst();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (MyStack.Count == 0)
+            {
+                throw new IndexOutOfRangeException("The stack is empty");
+            }
+
+            return MyStack.First.Value;
+        }
+
+        public void Add(T item)
+        {
+            Push(item);
+        }
+
+        public void Clear()
+        {
+            MyStack.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return MyStack.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return MyStack.GetEnumerator();
+        }
+    }
+}
